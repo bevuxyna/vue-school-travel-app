b4999c4025b41b152cb7c187927bc3c27ccfa6a7
vmelnikova <vmelnikova@phoenixit.ru>
2023-09-13 10:45:35 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <router-link to="/">Home</router-link>
+  <router-link to="/about">About</router-link>
+
+  <router-view></router-view>  
+</template>
+
+<style scoped>
+
+</style>

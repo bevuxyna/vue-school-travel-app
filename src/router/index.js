c815f04610561469d402b50d57505d1e25155710
vmelnikova <vmelnikova@phoenixit.ru>
2023-09-15 17:58:40 +0300

    customize the scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -45,6 +45,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior (to, from, savedPosition) {
+    return savedPosition || { top: 0 }
+  }
 });
 
 export default router;

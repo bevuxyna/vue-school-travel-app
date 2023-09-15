46ec2695a01fa9402ac8809f3fef20a816d9e75d
vmelnikova <vmelnikova@phoenixit.ru>
2023-09-15 17:42:37 +0300

    add route guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,50 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import sourceData from "@/data.json";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: "/",
+    name: "home",
+    component: Home,
   },
   {
-    path: '/:pathMatch(.*)*',
-    name: 'notFound',
-    component: () => import('@/views/NotFound.vue'),
+    path: "/:pathMatch(.*)*",
+    name: "notFound",
+    component: () => import("@/views/NotFound.vue"),
   },
   {
-    path: '/destination/:id/:slug',
-    name: 'destination',
-    component: () => import('@/views/Destination.vue'),
-    props: route => ({ ...route.params, id: parseInt(route.params.id )}),
+    path: "/destination/:id/:slug",
+    name: "destination",
+    component: () => import("@/views/Destination.vue"),
+    props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+    beforeEnter(to, from) {
+      const exists = sourceData.destinations.find(
+        (destination) => destination.id === parseInt(to.params.id)
+      );
+      if (!exists) {
+        return { 
+          name: "notFound",
+          params: { pathMatch: to.path.split('/').slice(1) },
+          query: to.query,
+          hash: to.hash
+        };
+      }
+    },
     children: [
       {
-        path: ':experienceSlug',
-        name: 'experience',
-        component: () => import('@/views/Experience.vue'),
-        props: route => ({ ...route.params, id: parseInt(route.params.id )})
-      }
-    ]
+        path: ":experienceSlug",
+        name: "experience",
+        component: () => import("@/views/Experience.vue"),
+        props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+      },
+    ],
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;

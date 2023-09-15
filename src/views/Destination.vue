04d5a8b04d58aadd13bd3aeb15e3648a3ef54082
vmelnikova <vmelnikova@phoenixit.ru>
2023-09-15 09:31:22 +0300

    add fetch

--- src/views/Destination.vue
+++ src/views/Destination.vue
@@ -1,23 +1,33 @@
 <script setup>
-import { computed } from "vue";
+import { computed, onBeforeMount, ref } from "vue";
 import { useRoute } from "vue-router";
-import sourceData from "@/data.json";
 
 const route = useRoute();
+const destination = ref(null);
 
 const denstinationId = computed(() => {
   return parseInt(route.params.id);
 });
 
-const destination = computed(() => {
-  return sourceData.destinations.find(
-    (destination) => destination.id === denstinationId.value
-  );
+// const destination = computed(() => {
+//   return sourceData.destinations.find(
+//     (destination) => destination.id === denstinationId.value
+//   );
+// });
+
+const getData =() => {
+  fetch(`https://travel-dummy-api.netlify.app/${route.params.slug}.json`)
+    .then((response) => response.json())
+    .then((data) => (destination.value = data));
+};
+
+onBeforeMount(() => {
+  destination.value = getData();
 });
 </script>
 
 <template>
-  <section class="destination">
+  <section v-if="destination" class="destination">
     <h2>{{ destination.name }}</h2>
     <div class="destination-details">
       <img :src="`/images/${destination.image}`" :alt="destination.name" />
